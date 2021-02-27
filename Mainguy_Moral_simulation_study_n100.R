690f59bfacf18b29405c047b7868cced3ee1f822
Rafael Moral <rafa_moral2004@yahoo.com.br>
2021-02-27 00:16:28 +0000

    added simulation studies using 100 datasets

---
+++ Mainguy_Moral_simulation_study_n100.R
@@ -0,0 +1,243 @@
+## GLM-derived fish mortality estimates and comparisons:
+## a proposed error structure selection approach for catch-curve analyses
+## Mainguy & Moral
+
+## This script reproduces the simulation studies in the paper
+
+## loading packages
+library(hnp)
+library(glmmTMB)
+library(tidyverse)
+library(gamlss)
+library(gridExtra)
+
+## loading helper functions
+source("helper_functions.R")
+
+## reading walleye dataset to serve as basis for simulation study
+walleye <- read.csv("walleyePP.csv", stringsAsFactors = TRUE, header = TRUE) %>%
+  mutate(YEAR = as.factor(YEAR)) %>%
+  filter(YEAR == "2012")
+
+## setting up true values
+## scenario 1: true model = equidispersion
+## scenario 2: true model = overdispersion (linear)
+## scenario 3: true model = overdispersion (quadratic)
+## scenario 4: true model = underdispersion
+
+true_beta <- round(coef(glm(N ~ AGE, family = poisson, data = walleye)), 1)
+true_disp_nb <- 2
+true_disp_dp <- .5
+
+n_sim <- 100
+beta <- true_beta
+age <- walleye$AGE
+
+## run studies for each scenario - takes approx. 50 hours in total
+#results1 <- run_study(age = age, beta = beta, disp = 1, n_sim = n_sim, family = "poisson")
+#results2 <- run_study(age = age, beta = beta, disp = 2, n_sim = n_sim, family = "nb1")
+#results3 <- run_study(age = age, beta = beta, disp = 2, n_sim = n_sim, family = "nb2")
+#results4 <- run_study(age = age, beta = beta, disp = .5, n_sim = n_sim, family = "dp")
+
+## compile results
+source("get_summaries_n100.R")
+
+aicc_all %>%
+  as_tibble %>%
+  mutate(model = rownames(aicc_all)) %>%
+  pivot_longer(cols = 1:4,
+               names_to = "scenario",
+               values_to = "aicc") %>%
+  ggplot(aes(x = model, y = aicc)) +
+  theme_bw() +
+  facet_wrap(~ scenario, scales = "free_y") +
+  geom_point()
+
+hnp_all %>%
+  as_tibble %>%
+  mutate(model = rownames(hnp_all)) %>%
+  pivot_longer(cols = 1:4,
+               names_to = "scenario",
+               values_to = "hnp") %>%
+  ggplot(aes(x = model, y = hnp)) +
+  theme_bw() +
+  facet_wrap(~ scenario, scales = "free_y") +
+  geom_point()
+
+rmse_slope_all %>%
+  as_tibble %>%
+  mutate(model = rownames(rmse_slope_all)) %>%
+  pivot_longer(cols = 1:4,
+               names_to = "scenario",
+               values_to = "rmse_slope") %>%
+  ggplot(aes(x = model, y = rmse_slope)) +
+  theme_bw() +
+  facet_wrap(~ scenario, scales = "free_y") +
+  geom_point()
+
+se_slope_all %>%
+  as_tibble %>%
+  mutate(model = rownames(se_slope_all)) %>%
+  pivot_longer(cols = 1:4,
+               names_to = "scenario",
+               values_to = "se_slope") %>%
+  ggplot(aes(x = model, y = se_slope)) +
+  theme_bw() +
+  facet_wrap(~ scenario, scales = "free_y") +
+  geom_point()
+
+###################### plots for paper
+
+fish1 <- read.csv("equi_fish_n100.csv", header = TRUE)
+fish2 <- read.csv("linear_fish_n100.csv", header = TRUE)
+fish3 <- read.csv("quad_fish_n100.csv", header = TRUE)
+fish4 <- read.csv("under_fish_n100.csv", header = TRUE)
+
+fish1$scenario <- "equidispersion"
+fish2$scenario <- "linear overdispersion"
+fish3$scenario <- "quadratic overdispersion"
+fish4$scenario <- "underdispersion"
+
+fish <- rbind(fish1, fish2, fish3, fish4)
+
+fish_est <- fish %>%
+  filter(type == "estimate") %>%
+  dplyr::select(- type) %>%
+  pivot_longer(cols = 1:6,
+               names_to = "model",
+               values_to = "slope") %>%
+  dplyr::select(model, slope, scenario)
+
+all_slopes <- rbind(slope1, slope2, slope3, slope4)
+
+model_est <- - all_slopes %>%
+  t %>%
+  as_tibble %>%
+  pivot_longer(cols = 1:28,
+               names_to = "model",
+               values_to = "slope") %>%
+  filter(model != "pn" & model != "pn1" & model != "pn2" & model != "pn3")
+model_est$model <- as.factor(model_est$model)
+levels(model_est$model) <- rep(c("CMP","GP","NB1","NB2","Poisson","QP"), each = 4)
+model_est$scenario <- gl(4, 6, labels = c("equidispersion",
+                                          "linear overdispersion",
+                                          "quadratic overdispersion",
+                                          "underdispersion"))
+
+fish_est$method <- "fishmethods"
+model_est$method <- "GLM"
+all_est <- rbind(fish_est, model_est)
+all_est$model <- factor(all_est$model, levels = c("LR","WLR","Heincke","CR","CRCB","PM",
+                                                  "Poisson","QP","NB1","NB2","CMP","GP"))
+
+fish_se <- fish %>%
+  filter(type == "standard_error") %>%
+  dplyr::select(- type) %>%
+  pivot_longer(cols = 1:6,
+               names_to = "model",
+               values_to = "se") %>%
+  dplyr::select(model, se, scenario)
+
+all_ses <- rbind(se1_slo, se2_slo, se3_slo, se4_slo)
+rownames(all_ses) <- substr(rownames(all_ses), 1, nchar(rownames(all_ses)) - 4)
+
+model_se <- all_ses %>%
+  t %>%
+  as_tibble
+colnames(model_se) <- c(colnames(model_se)[1:7], paste0(colnames(model_se)[1:7], "1"),
+                        paste0(colnames(model_se)[1:7], "2"), paste0(colnames(model_se)[1:7], "3"))
+
+model_se <- model_se %>%
+  pivot_longer(cols = 1:28,
+               names_to = "model",
+               values_to = "se") %>%
+  filter(model != "pn" & model != "pn1" & model != "pn2" & model != "pn3")
+model_se$model <- as.factor(model_se$model)
+levels(model_se$model) <- rep(c("CMP","GP","NB1","NB2","Poisson","QP"), each = 4)
+model_se$scenario <- gl(4, 6, labels = c("equidispersion",
+                                         "linear overdispersion",
+                                         "quadratic overdispersion",
+                                         "underdispersion"))
+
+fish_se$method <- "fishmethods"
+model_se$method <- "GLM"
+all_se <- rbind(fish_se, model_se)
+all_se$model <- factor(all_se$model, levels = c("LR","WLR","Heincke","CR","CRCB","PM",
+                                                "Poisson","QP","NB1","NB2","CMP","GP"))
+
+fake_data <- data.frame(scenario = rep(c("Equi-dispersion","Linear over-dispersion","Quadratic over-dispersion","Under-dispersion"), 2),
+                        model = "LR",
+                        method = "fishmethods",
+                        slope = c(.2,.2,-.2,.2,.7,.7,1.8,.7))
+
+all_est$scenario <- as.factor(all_est$scenario)
+levels(all_est$scenario) <- c("Equi-dispersion","Linear over-dispersion",
+                              "Quadratic over-dispersion","Under-dispersion")
+all_se$scenario <- as.factor(all_se$scenario)
+levels(all_se$scenario) <- c("Equi-dispersion","Linear over-dispersion",
+                             "Quadratic over-dispersion","Under-dispersion")
+
+weighted_se <- data.frame(scenario = c("Equi-dispersion","Linear over-dispersion","Quadratic over-dispersion","Under-dispersion"),
+                          wse = c(0.02470705,0.03689285,0.09015703,0.01900242))
+                                 
+p1 <- all_est %>%
+  ggplot(aes(x = model, y = slope, fill = method)) +
+  theme_bw() +
+  facet_wrap(~ scenario, scales = "free_y") +
+  #geom_hline(yintercept = 0.5, col = "lightgray", lty = 1, lwd = 1) +
+  geom_boxplot(outlier.size = .75, outlier.alpha = .5) +
+  geom_hline(yintercept = 0.5, lty = "11", lwd = 1) +
+  ylab(expression(italic(hat(Z)))) +
+  xlab("") +
+  theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust = 1, color = 1),
+        axis.text.y = element_text(color = 1),
+        panel.border = element_rect(colour = 1, fill = NA),
+        panel.background = element_rect(colour = 1, size = 1),
+        strip.background = element_rect(colour = 1, size = 1),
+        legend.position = "none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.text.x = element_text(face = "bold", size = 10)) +
+  scale_fill_grey(start = .7, end = 1) +
+  ggtitle("(a)") +
+  geom_point(data = fake_data, col = NA)
+
+p2 <- all_se %>%
+  ggplot(aes(x = model, y = se, fill = method)) +
+  theme_bw() +
+  facet_wrap(~ scenario, scales = "free_y") +
+  #geom_hline(data = weighted_se,
+  #           aes(yintercept = wse),
+  #           col = "lightgray",
+  #           lty = 1, lwd = 1) +
+  geom_boxplot(outlier.size = .75, outlier.alpha = .5) +
+  ylab(expression(paste("SE(",italic(hat(Z)),")"))) +
+  xlab("") +
+  theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust = 1, color = 1),
+        axis.text.y = element_text(color = 1),
+        panel.border = element_rect(colour = 1, fill = NA),
+        panel.background = element_rect(colour = 1, size = 1),
+        strip.background = element_rect(colour = 1, size = 1),
+        legend.position = "none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.text.x = element_text(face = "bold", size = 10)) +
+  scale_fill_grey(start = .7, end = 1) +
+  ggtitle("(b)") +
+  geom_hline(data = weighted_se,
+             aes(yintercept = wse),
+             lty = "11", lwd = 1)
+
+png("figure1.tiff", res = 800, units = "in", w = 10, h = 12)
+grid.arrange(p1, p2, ncol = 1)
+dev.off()
+
+png("figure1a.tiff", res = 800, units = "in", w = 10, h = 6)
+p1 + ggtitle("")
+dev.off()
+
+png("figure1b.tiff", res = 800, units = "in", w = 10, h = 6)
+p2 + ggtitle("")
+dev.off()
+
+#save.image("results_n100.RData")
---
+++ get_summaries_n100.R
@@ -0,0 +1,261 @@
+load("results_n100_updated.RData")
+
+AICc <- function(obj) {
+  aic <- AIC(obj)
+  if(class(obj)[1] == "glm") {
+    np <- length(obj$coef)
+    n <- nrow(obj$data)
+    if(obj$family$family == "quasipoisson") {
+      np <- np + 1
+    }
+  } else {
+    np <- length(obj$fit$par)
+    n <- nrow(obj$frame)
+  }
+  aicc <- aic + (2 * np^2 + 2 * np)/(n - np - 1)
+  return(aicc)
+}
+
+aic_summary1 <- do.call(data.frame, results1$aic)
+colnames(aic_summary1) <- 1:n_sim
+aic_summary1[aic_summary1 < 0] <- NA
+aic1 <- apply(aic_summary1, 1, mean, na.rm = TRUE)
+
+aic_summary2 <- do.call(data.frame, results2$aic)
+colnames(aic_summary2) <- 1:n_sim
+aic_summary2[aic_summary2 < 0] <- NA
+aic2 <- apply(aic_summary2, 1, mean, na.rm = TRUE)
+
+aic_summary3 <- do.call(data.frame, results3$aic)
+colnames(aic_summary3) <- 1:n_sim
+aic_summary3[aic_summary3 < 0] <- NA
+aic3 <- apply(aic_summary3, 1, mean, na.rm = TRUE)
+
+aic_summary4 <- do.call(data.frame, results4$aic)
+colnames(aic_summary4) <- 1:n_sim
+aic_summary4[aic_summary4 < 0] <- NA
+aic4 <- apply(aic_summary4, 1, mean, na.rm = TRUE)
+
+results1$aicc <- lapply(results1$models,
+                        function(x) unlist(lapply(x,
+                                                  function(y) AICc(y))))
+results2$aicc <- lapply(results2$models,
+                        function(x) unlist(lapply(x,
+                                                  function(y) AICc(y))))
+results3$aicc <- lapply(results3$models,
+                        function(x) unlist(lapply(x,
+                                                  function(y) AICc(y))))
+results4$aicc <- lapply(results4$models,
+                        function(x) unlist(lapply(x,
+                                                  function(y) AICc(y))))
+
+aicc_summary1 <- do.call(data.frame, results1$aicc)
+colnames(aicc_summary1) <- 1:n_sim
+aicc_summary1[aicc_summary1 < 0] <- NA
+aicc1 <- apply(aicc_summary1, 1, mean, na.rm = TRUE)
+
+aicc_summary2 <- do.call(data.frame, results2$aicc)
+colnames(aicc_summary2) <- 1:n_sim
+aicc_summary2[aicc_summary2 < 0] <- NA
+aicc2 <- apply(aicc_summary2, 1, mean, na.rm = TRUE)
+
+aicc_summary3 <- do.call(data.frame, results3$aicc)
+colnames(aicc_summary3) <- 1:n_sim
+aicc_summary3[aicc_summary3 < 0] <- NA
+aicc3 <- apply(aicc_summary3, 1, mean, na.rm = TRUE)
+
+aicc_summary4 <- do.call(data.frame, results4$aicc)
+colnames(aicc_summary4) <- 1:n_sim
+aicc_summary4[aicc_summary4 < 0] <- NA
+aicc4 <- apply(aicc_summary4, 1, mean, na.rm = TRUE)
+
+hnp_summary1 <- do.call(data.frame, results1$hnp)
+colnames(hnp_summary1) <- 1:n_sim
+na_index <- is.na(aic_summary1)
+na_index[1:2,] <- FALSE
+hnp_summary1[na_index] <- NA
+hnp1 <- apply(hnp_summary1, 1, mean, na.rm = TRUE)
+
+hnp_summary2 <- do.call(data.frame, results2$hnp)
+colnames(hnp_summary2) <- 1:n_sim
+na_index <- is.na(aic_summary2)
+na_index[1:2,] <- FALSE
+hnp_summary2[na_index] <- NA
+hnp2 <- apply(hnp_summary2, 1, mean, na.rm = TRUE)
+
+hnp_summary3 <- do.call(data.frame, results3$hnp)
+colnames(hnp_summary3) <- 1:n_sim
+na_index <- is.na(aic_summary3)
+na_index[1:2,] <- FALSE
+hnp_summary3[na_index] <- NA
+hnp3 <- apply(hnp_summary3, 1, mean, na.rm = TRUE)
+
+hnp_summary4 <- do.call(data.frame, results4$hnp)
+colnames(hnp_summary4) <- 1:n_sim
+na_index <- is.na(aic_summary4)
+na_index[1:2,] <- FALSE
+hnp_summary4[na_index] <- NA
+hnp4 <- apply(hnp_summary4, 1, mean, na.rm = TRUE)
+
+coef_all1 <- do.call(data.frame, lapply(results1$coef, function(x) t(x[-3,])))
+coef_all2 <- do.call(data.frame, lapply(results2$coef, function(x) t(x[-3,])))
+coef_all3 <- do.call(data.frame, lapply(results3$coef, function(x) t(x[-3,])))
+coef_all4 <- do.call(data.frame, lapply(results4$coef, function(x) t(x[-3,])))
+
+intercept1 <- coef_all1[,seq(1,39,2)]
+intercept2 <- coef_all2[,seq(1,39,2)]
+intercept3 <- coef_all3[,seq(1,39,2)]
+intercept4 <- coef_all4[,seq(1,39,2)]
+slope1 <- coef_all1[,seq(2,40,2)]
+slope2 <- coef_all2[,seq(2,40,2)]
+slope3 <- coef_all3[,seq(2,40,2)]
+slope4 <- coef_all4[,seq(2,40,2)]
+
+est_intercept1 <- apply(intercept1, 1, mean)
+est_intercept2 <- apply(intercept2, 1, mean)
+est_intercept3 <- apply(intercept3, 1, mean)
+est_intercept4 <- apply(intercept4, 1, mean)
+est_slope1 <- apply(slope1, 1, mean)
+est_slope2 <- apply(slope2, 1, mean)
+est_slope3 <- apply(slope3, 1, mean)
+est_slope4 <- apply(slope4, 1, mean)
+
+sd_intercept1 <- apply(intercept1, 1, sd)
+sd_intercept2 <- apply(intercept2, 1, sd)
+sd_intercept3 <- apply(intercept3, 1, sd)
+sd_intercept4 <- apply(intercept4, 1, sd)
+sd_slope1 <- apply(slope1, 1, sd)
+sd_slope2 <- apply(slope2, 1, sd)
+sd_slope3 <- apply(slope3, 1, sd)
+sd_slope4 <- apply(slope4, 1, sd)
+
+bias_intercept1 <- apply(intercept1 - true_beta[1], 1, mean)
+bias_intercept2 <- apply(intercept2 - true_beta[1], 1, mean)
+bias_intercept3 <- apply(intercept3 - true_beta[1], 1, mean)
+bias_intercept4 <- apply(intercept4 - true_beta[1], 1, mean)
+bias_slope1 <- apply(slope1 - true_beta[2], 1, mean)
+bias_slope2 <- apply(slope2 - true_beta[2], 1, mean)
+bias_slope3 <- apply(slope3 - true_beta[2], 1, mean)
+bias_slope4 <- apply(slope4 - true_beta[2], 1, mean)
+
+mse_intercept1 <- apply((intercept1 - true_beta[1])^2, 1, sum)
+mse_intercept2 <- apply((intercept2 - true_beta[1])^2, 1, sum)
+mse_intercept3 <- apply((intercept3 - true_beta[1])^2, 1, sum)
+mse_intercept4 <- apply((intercept4 - true_beta[1])^2, 1, sum)
+rmse_intercept1 <- sqrt(mse_intercept1)
+rmse_intercept2 <- sqrt(mse_intercept2)
+rmse_intercept3 <- sqrt(mse_intercept3)
+rmse_intercept4 <- sqrt(mse_intercept4)
+mse_slope1 <- apply((slope1 - true_beta[2])^2, 1, sum)
+mse_slope2 <- apply((slope2 - true_beta[2])^2, 1, sum)
+mse_slope3 <- apply((slope3 - true_beta[2])^2, 1, sum)
+mse_slope4 <- apply((slope4 - true_beta[2])^2, 1, sum)
+rmse_slope1 <- sqrt(mse_slope1)
+rmse_slope2 <- sqrt(mse_slope2)
+rmse_slope3 <- sqrt(mse_slope3)
+rmse_slope4 <- sqrt(mse_slope4)
+
+get_se <- function(obj) {
+  if(class(obj)[1] == "glm") {
+    return(summary(obj)$coefficients[,2])
+  } else {
+    return(summary(obj)$coefficients$cond[,2])
+  }
+}
+
+se1 <- lapply(results1$models, function(x) unlist(lapply(x, get_se)))
+se2 <- lapply(results2$models, function(x) unlist(lapply(x, get_se)))
+se3 <- lapply(results3$models, function(x) unlist(lapply(x, get_se)))
+se4 <- lapply(results4$models, function(x) unlist(lapply(x, get_se)))
+se1_all <- do.call(cbind, se1)
+se2_all <- do.call(cbind, se2)
+se3_all <- do.call(cbind, se3)
+se4_all <- do.call(cbind, se4)
+se1_int <- se1_all[seq(1,13,2),]
+se2_int <- se2_all[seq(1,13,2),]
+se3_int <- se3_all[seq(1,13,2),]
+se4_int <- se4_all[seq(1,13,2),]
+se1_slo <- se1_all[seq(2,14,2),]
+se2_slo <- se2_all[seq(2,14,2),]
+se3_slo <- se3_all[seq(2,14,2),]
+se4_slo <- se4_all[seq(2,14,2),]
+se_intercept1 <- apply(se1_int, 1, function(x) mean(na.omit(x)))
+se_intercept2 <- apply(se2_int, 1, function(x) mean(na.omit(x)))
+se_intercept3 <- apply(se3_int, 1, function(x) mean(na.omit(x)))
+se_intercept4 <- apply(se4_int, 1, function(x) mean(na.omit(x)))
+se_slope1 <- apply(se1_slo, 1, function(x) mean(na.omit(x)))
+se_slope2 <- apply(se2_slo, 1, function(x) mean(na.omit(x)))
+se_slope3 <- apply(se3_slo, 1, function(x) mean(na.omit(x)))
+se_slope4 <- apply(se4_slo, 1, function(x) mean(na.omit(x)))
+
+aic_all <- cbind(aic1, aic2, aic3, aic4)
+aicc_all <- cbind(aicc1, aicc2, aicc3, aicc4)
+hnp_all <- cbind(hnp1, hnp2, hnp3, hnp4)
+intercept_all <- cbind(est_intercept1, est_intercept2, est_intercept3, est_intercept4)
+slope_all <- cbind(est_slope1, est_slope2, est_slope3, est_slope4)
+sd_intercept_all <- cbind(sd_intercept1, sd_intercept2, sd_intercept3, sd_intercept4)
+sd_slope_all <- cbind(sd_slope1, sd_slope2, sd_slope3, sd_slope4)
+bias_intercept_all <- cbind(bias_intercept1, bias_intercept2, bias_intercept3, bias_intercept4)
+bias_slope_all <- cbind(bias_slope1, bias_slope2, bias_slope3, bias_slope4)
+mse_intercept_all <- cbind(mse_intercept1, mse_intercept2, mse_intercept3, mse_intercept4)
+mse_slope_all <- cbind(mse_slope1, mse_slope2, mse_slope3, mse_slope4)
+rmse_intercept_all <- cbind(rmse_intercept1, rmse_intercept2, rmse_intercept3, rmse_intercept4)
+rmse_slope_all <- cbind(rmse_slope1, rmse_slope2, rmse_slope3, rmse_slope4)
+se_intercept_all <- cbind(se_intercept1, se_intercept2, se_intercept3, se_intercept4)
+se_slope_all <- cbind(se_slope1, se_slope2, se_slope3, se_slope4)
+
+colnames(aic_all) <- colnames(aicc_all) <- colnames(hnp_all) <- 
+  colnames(intercept_all) <- colnames(slope_all) <- colnames(bias_intercept_all) <-
+  colnames(bias_slope_all) <- colnames(mse_intercept_all) <- colnames(mse_slope_all) <-
+  colnames(rmse_intercept_all) <- colnames(rmse_slope_all) <- 
+  colnames(se_intercept_all) <- colnames(se_slope_all) <- c("equidisp","linear_overdisp","quad_overdisp","underdisp")
+
+rownames(se_intercept_all) <- rownames(se_slope_all) <- rownames(rmse_intercept_all)
+
+## getting AICc weights
+# example using package MuMIn
+#library(MuMIn)
+#model.sel(object = results1$models[[1]][c(1,5,6)])
+
+# example doing it by hand
+#my_aicc <- na.omit(aicc_summary1[-7,1]) %>% as.numeric
+#delta_aicc <- my_aicc - min(my_aicc)
+#exp(-.5 * delta_aicc)/sum(exp(-.5 * delta_aicc))
+
+get_aicc_weights <- function(aicc_values) {
+  get_nas <- is.na(aicc_values)
+  aicc_values2 <- na.omit(aicc_values) %>% as.numeric
+  delta_aicc <- aicc_values2 - min(aicc_values2)
+  weights <- exp(-.5 * delta_aicc)/sum(exp(-.5 * delta_aicc))
+  aicc_weights <- rep(0, length(aicc_values))
+  aicc_weights[!get_nas] <- weights
+  return(aicc_weights)
+}
+
+all_weights1 <- apply(aicc_summary1[-7,], 2, get_aicc_weights)
+all_weights2 <- apply(aicc_summary2[-7,], 2, get_aicc_weights)
+all_weights3 <- apply(aicc_summary3[-7,], 2, get_aicc_weights)
+all_weights4 <- apply(aicc_summary4[-7,], 2, get_aicc_weights)
+
+all_weights <- cbind(rowMeans(all_weights1),
+                     rowMeans(all_weights2),
+                     rowMeans(all_weights3),
+                     rowMeans(all_weights4))
+colnames(all_weights) <- c("equi","lin","quad","under")
+rownames(all_weights) <- c("poisson","qp","nb1","nb2","comp","gp")
+
+all_weights
+
+### total counts (sum y variable for each simulated dataset)
+sum_y <- cbind(sapply(results1$data, function(x) sum(x$y)),
+               sapply(results2$data, function(x) sum(x$y)),
+               sapply(results3$data, function(x) sum(x$y)),
+               sapply(results4$data, function(x) sum(x$y)))
+
+apply(sum_y, 2, mean)
+apply(sum_y, 2, sd)
+apply(sum_y, 2, range)
+
+mean(sum_y)
+sd(sum_y)
+range(sum_y)
